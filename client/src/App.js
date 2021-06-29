7a50df379aaf0264208083fad78d9eb8608b36d3
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-30 01:11:32 +0530

    client side setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
+import React from 'react'
+import { MuiThemeProvider, createMuiTheme, responsiveFontSizes, Container, Paper } from '@material-ui/core'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
+
 const App = () => {
+
+  const theme = responsiveFontSizes(
+    createMuiTheme({
+      typography: {
+        fontFamily: '"Ubuntu"',
+      },
+      palette: {
+        type: 'dark',
+        primary: { main: '#8fd9a8', secondary: '#7eca9c'}, 
+        secondary: { main: '#ffffff' },
+        alternate: { main: '#161616', secondary: '#393e46'},
+      },
+    })
+  )
+
   return (
-    <div>
-      Chat App
-    </div>
+    <>
+    <MuiThemeProvider theme={theme}>
+    <Paper>
+      <Router>
+          <Route path='/' exact component={Join} />
+          <Route path='/chat' component={Chat} />
+      </Router>
+    </Paper>
+    </MuiThemeProvider>
+    </>
   );
 }
 
-export default App;
+export default App
