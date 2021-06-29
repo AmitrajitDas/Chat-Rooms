c21b4a566f13868efca793d3ca562a096d3c45cd
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-30 01:12:58 +0530

    server side structuring

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-// templating //
+import express from 'express'
+import cors from 'cors'
+import { Server } from 'socket.io'
+import http from 'http'
+import colors from 'colors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const httpServer = http.createServer(app)
+const io = new Server(httpServer)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server has started in ${PORT}`.yellow.bold))
+
