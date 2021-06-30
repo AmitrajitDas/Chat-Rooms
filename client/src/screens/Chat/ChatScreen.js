f1a07a2cb187577298f3a3020ffcb7d599253348
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-30 23:59:10 +0530

    emitting sockets from client side

--- client/src/screens/Chat/ChatScreen.js
+++ client/src/screens/Chat/ChatScreen.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => {
+import { useStyles } from './styles'
+
+let socket
+
+const Chat = ({ location }) => {
+
+    const classes = useStyles()
+    const [username, setUsername] = useState('')
+    const [room, setRoom] = useState('')
+
+    const ENDPOINT = 'localhost:5000'
+
+    useEffect(() => {
+        const { username, room } = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
+
+        setUsername(username)
+        setRoom(room)
+
+        socket.emit('join', {username, room}, () => {
+
+        })
+
+        return () => {
+
+            socket.emit('disconnect')
+            socket.off()
+        }
+
+    }, [location.search, ENDPOINT])
+    
+    
     return (
         <h1>
             Chat
