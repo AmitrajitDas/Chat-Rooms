ac5399eb94f52b3026d6c92c811476af7e8da77f
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-30 20:48:03 +0530

    sockets cors issue fixed

--- server/index.js
+++ server/index.js
@@ -10,11 +10,19 @@ import homeRoute from './routes/homeRoute.js'
 dotenv.config()
 
 const app = express()
+app.use(cors())
+
 const httpServer = http.createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+    cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+})
 
-
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
 
     console.log('New connection established'.bgGreen)
 
@@ -28,5 +36,5 @@ app.use(homeRoute)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server has started in ${PORT}`.yellow.bold))
+httpServer.listen(PORT, () => console.log(`Server has started in ${PORT}`.yellow.bold))
 
