d85dcf1c47b4765abca6c1429f16e521753d2aa9
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-30 23:06:23 +0530

    mongodb connected

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
