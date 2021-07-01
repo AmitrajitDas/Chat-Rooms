df4a7b13ea79200be68f98aa7ff01e1eb0d7ddca
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-01 19:05:07 +0530

    emoji support

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactEmoji from 'react-emoji';
 
 import  './styles.css'
 
@@ -18,7 +19,7 @@ const Message = ({ username, message: {user,text} }) => {
         <div className="messageContainer justifyEnd">
           <p className="sentText pr-10">{trimmedUsername}</p>
           <div className="messageBox backgroundBlue">
-            <p className="messageText colorWhite">{text}</p>
+            <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
           </div>
         </div>
 
@@ -26,7 +27,7 @@ const Message = ({ username, message: {user,text} }) => {
         : (
             <div className="messageContainer justifyStart">
             <div className="messageBox backgroundLight">
-              <p className="messageText colorDark">{text}</p>
+              <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
             </div>
             <p className="sentText pl-10 ">{user}</p>
           </div>
