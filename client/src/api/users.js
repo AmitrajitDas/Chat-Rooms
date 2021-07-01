c217ab9a5385d3a69c9e25bcda01088c647fd0f2
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-02 02:39:24 +0530

    updated

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: `http://localhost:5000/api/auth` })
+const API = axios.create({ baseURL: 'https://chat-rooms-backend.herokuapp.com/api/auth' })
 
 const config = {
 
--- client/src/screens/Chat/ChatScreen.js
+++ client/src/screens/Chat/ChatScreen.js
@@ -20,7 +20,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState([])
 
-    const ENDPOINT = 'localhost:5000'
+    const ENDPOINT = 'https://chat-rooms-backend.herokuapp.com/'
 
     useEffect(() => {
         const { username, room } = queryString.parse(location.search)
